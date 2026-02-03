3553e56fda8273e4c9a90e76248806c7cf981730
Lamia <lamyax66@gmail.com>
2026-02-03 10:58:01 +0300

    feat: reactive form

--- reactiveForm/src/app/app.ts
+++ reactiveForm/src/app/app.ts
@@ -1,11 +1,26 @@
 import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('reactiveForm');
+
+  userForm: FormGroup = new FormGroup({
+    FullName: new FormControl("",[Validators.required,Validators.minLength(3)]),
+    Email: new FormControl("",[Validators.required,Validators.email]),
+    Password: new FormControl("",[Validators.required,Validators.minLength(8)]),
+    Age: new FormControl("",[Validators.required,Validators.min(18)]),
+    acceptTerms: new FormControl(false, [Validators.requiredTrue])
+  });
+
+  onSubmit() {
+  if (this.userForm.valid) {
+    console.log(this.userForm.value);
+  }
+}
 }
